7cfcdac71cbb1f32114f14a9bd11717ebe5f9f33
jayKrishnamurthy <jayasimhan.k@gmail.com>
2019-05-15 01:29:26 -0700

    Udacity Nanodegree loganalysis project

---
+++ README.md
@@ -0,0 +1,30 @@
+Logs Analysis Project - Udacity Full Stack Web Developer Nanodegree
+DESCRIPTION
+Build an internal reporting tool that will use information from the database to discover what kind of articles the site's readers like. The program can be run from the command line. It won't take any input from the user. Instead, it will connect to the postgreSQL database running on virtual machine and print out the answers to the below 3 questions.
+
+What are the most popular three articles of all time?
+Who are the most popular article authors of all time?
+On which days did more than 1% of requests lead to errors?
+
+RUNNING THE PROGRAM
+To get started, Install the virtual machine and Install vagrant. Detailed instructions can be found [here](https://github.com/udacity/fullstack-nanodegree-vm). Use vagrant up to bring the virtual machine online and vagrant ssh to login.
+
+Download the data provided by Udacity here. Unzip the file in order to extract newsdata.sql. This file should be inside the Vagrant folder.
+
+Load the database using psql -d news -f newsdata.sql.
+
+Connect to the database using psql -d news.
+
+Create the Views given below. Then exit psql.
+
+Now execute the Python file - python logs_analysis.py.
+
+CREATE THE FOLLOWING VIEWS FOR QUESTION 3:
+
+CREATE VIEW ErrorLogs AS SELECT to_char(time::date, 'Mon dd yyyy') as logTimestamp, count(*) as logCount FROM log 
+WHERE status='404 NOT FOUND' 
+group by to_char(time::date,'Mon dd yyyy');
+
+CREATE VIEW SuccessLogs AS SELECT to_char(time::date, 'Mon dd yyyy') as logTimestamp, count(*) as logCount FROM log 
+WHERE status='200 OK' 
+group by to_char(time::date,'Mon dd yyyy');
---
+++ loganalysis.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+#log analysis project
+
+import psycopg2
+
+DB_Name = "news"
+
+# define sql queries
+popularArticles_sql = """select Replace(path,'/article/','') as Article_Name,
+                        count(*) as Total_ArticleViews from log
+                        where path <> '/' group by path order by count(*) desc limit 3;"""
+
+populatArticleAuthors_sql = """select authors.name as Author_Name, 
+                                count(*) as Total_ArticleViews_PerAuthor 
+                                from articles inner join authors 
+                                on authors.id = articles.author 
+                                inner join log on Replace(log.path,'/article/','') = articles.slug 
+                                where log.path <> '/' group by authors.name 
+                                order by count(*) desc;"""
+
+failurePercentage_sql = """select ErrorLogs.logTimestamp as Error_Date,
+                           cast((ErrorLogs.logCount::decimal/(ErrorLogs.logCount::decimal+ SuccessLogs.logCount::decimal)) * 100 as decimal(18,2)) as FailPercentage 
+                           from ErrorLogs join SuccessLogs 
+                           on ErrorLogs.logTimestamp = SuccessLogs.logTimestamp 
+                           where cast((ErrorLogs.logCount::decimal/(ErrorLogs.logCount::decimal+ SuccessLogs.logCount::decimal)) * 100 as decimal(18,2)) > 1;"""
+
+
+def executeSql(sqlQuery):
+    conn = psycopg2.connect(database=DB_Name)
+    cursor = conn.cursor()
+    cursor.execute(sqlQuery)
+    results = cursor.fetchall()
+    conn.close()
+    return results
+
+
+def popularArticles(sqlQuery):
+    results = executeSql(sqlQuery)
+    #format and print results
+    print('\n 1. Most popular three articles of all time: \n')
+    for row in results:
+        print('\t' + str(row[0]) + '--' + str(row[1]) + ' views')
+
+
+def popularArticleAuthors(sqlQuery):
+    results = executeSql(sqlQuery)
+    #format and print results
+    print('\n 2. Most popular article authors of all time: \n')
+    for row in results:
+        print('\t' + str(row[0]) + '--' + str(row[1]) + ' views')
+
+def failurePercentage(sqlQuery):
+    results = executeSql(sqlQuery)
+    #format and print results
+    print('\n 3. More than 1percent of errors On: \n')
+    for row in results:
+        print('\t' + str(row[0]) + '--' + str(row[1]) + '%' + ' errors')
+
+
+if __name__ == '__main__':
+    #execute queries and print
+    popularArticles(popularArticles_sql)
+    popularArticleAuthors(populatArticleAuthors_sql)
+    failurePercentage(failurePercentage_sql)
